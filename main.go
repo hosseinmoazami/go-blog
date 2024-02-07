a9d7c76099d8a12c14cc49a606c09c6de5a17c92
Hossein <hossein.moazami0662@gmail.com>
2024-02-07 14:08:37 +0330

    add viper package to manage config files, implement configs for host, port, name

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"blog/config"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	configs := configSet()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message":  "pong",
+			"app name": viper.GetString("App.Name"),
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the configs")
+	}
+
+	var configs config.Config
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	return configs
 }
