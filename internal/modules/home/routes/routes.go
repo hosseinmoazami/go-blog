f30816c750d06a13641a70b3ea341aefcfbb506d
Hossein <hossein.moazami0662@gmail.com>
2024-02-07 19:34:55 +0330

    add controller for render routes

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,21 +1,12 @@
 package routes
 
 import (
-	"blog/pkg/html"
+	homeCtrl "blog/internal/modules/home/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/", func(c *gin.Context) {
-		html.Render(c, 200, "modules/home/html/home", gin.H{
-			"title": "Home Page",
-		})
-	})
-
-	router.GET("/about", func(c *gin.Context) {
-		html.Render(c, 200, "modules/home/html/about", gin.H{
-			"title": "About Page",
-		})
-	})
+	homeController := homeCtrl.New()
+	router.GET("/", homeController.Index)
 }
