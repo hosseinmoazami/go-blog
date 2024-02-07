5a7012bae099160989675d19450e5e705e78b457
Hossein <hossein.moazami0662@gmail.com>
2024-02-07 16:22:53 +0330

    implement html render file, add middle parameter for return html file values

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,17 +1,21 @@
 package routes
 
 import (
-	"net/http"
+	"blog/pkg/html"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func Routes(router *gin.Engine) {
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":  "pong",
-			"app name": viper.GetString("App.Name"),
+		html.Render(c, 200, "modules/home/html/home", gin.H{
+			"title": "Home Page",
+		})
+	})
+
+	router.GET("/about", func(c *gin.Context) {
+		html.Render(c, 200, "modules/home/html/about", gin.H{
+			"title": "About Page",
 		})
 	})
 }
