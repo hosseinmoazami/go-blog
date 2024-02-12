da6c1091fded51d5006edb4187e903ca95f95522
Hossein <hossein.moazami0662@gmail.com>
2024-02-12 16:53:59 +0330

    add dynamic data to render home page

--- internal/modules/home/controllers/home_controller.go
+++ internal/modules/home/controllers/home_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	ArticleService "blog/internal/modules/article/services"
+	"blog/pkg/html"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,13 +19,16 @@ func New() *Controller {
 }
 
 func (controller *Controller) Index(c *gin.Context) {
-	// html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-	// 	"title": "Home Page",
-	// })
-	// c.JSON(http.StatusOK, controller.articleRepository.List(1))
-
-	c.JSON(http.StatusOK, gin.H{
+	html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+		"title":    "Home Page",
 		"featured": controller.articleService.GetFeaturedArticles(),
 		"stories":  controller.articleService.GetStoriesArticles(),
 	})
+
+	// c.JSON(http.StatusOK, controller.articleRepository.List(1))
+
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"featured": controller.articleService.GetFeaturedArticles(),
+	// 	"stories":  controller.articleService.GetStoriesArticles(),
+	// })
 }
