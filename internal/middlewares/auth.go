ff47d774ef68ee31ee0db94f414cbbe21bbc1bf8
Hossein <hossein.moazami0662@gmail.com>
2024-02-14 14:42:44 +0330

    add middleware for authenticate use logged in and guest user

---
+++ internal/middlewares/auth.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	userRepository "blog/internal/modules/user/repositories"
+	"blog/pkg/sessions"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IsAuth() gin.HandlerFunc {
+	userRepo := userRepository.New()
+	return func(c *gin.Context) {
+		authID := sessions.Get(c, "auth")
+		userID, _ := strconv.Atoi(authID)
+
+		user := userRepo.FindByID(userID)
+
+		if user.ID == 0 {
+			c.Redirect(http.StatusFound, "/login")
+			return
+		}
+
+		c.Next()
+	}
+}
